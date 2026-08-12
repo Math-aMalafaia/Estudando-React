7e3d2074a155a18cedeb319b359f1adc342d242b
Math.aMalafaia <math.amalafaia@gmail.com>
2026-08-11 21:56:27 -0300

    Feat: estudando React, testando botãos e algumas funçoes. Docs: Existe um erro sobre uma mudação de variavel dinamica, esta para corrigir na proxima atualização

--- magic-react/src/Conteudo.tsx
+++ magic-react/src/Conteudo.tsx
@@ -1,10 +1,58 @@
 function Conteudo(){
+
+    function ClickButton(){
+        alert("Parabéns você clicou no botão")
+    }
+
+    const ClickButton2 = () => {
+        alert("Parabéns você clicou no segundo Botão")
+    }
+
+    let Nome = "Sem nada"
     return(
         <>
             <main>
                 <h2>Meu nome é Matheus Gomes</h2>
                 <h3>Sou Desenvolvedor & psicologo</h3>
                 <a href="https://github.com/Math-aMalafaia/Estudando-React">Github</a>
+
+                
+                
+                <button onClick={ClickButton}>
+                    Clique para mostrar Matheus Gomes
+                </button>
+                
+                <button onClick={ClickButton2}>
+                    Clique para mostrar Matheus Gomes
+                </button>
+                
+                <button onClick={
+                    () => {
+                        alert("Parabéns você clicou no terceiro botão")
+                    }
+                }>
+                    Clique para mostrar Matheus Gomes
+                </button>
+
+                <button onClick={
+                    () => {
+                        Nome = "Parabéns você clicou no quarto botão"
+                    }
+                }>
+                    Clique para mostra Matheus Gomes
+                </button>
+
+                <button onMouseOver={
+                    () => {
+                        alert("Mouse por cima do botão")
+                    }
+                } >
+                    Clique para mostra Matheus Gomes
+                </button>
+
+                <p>
+                    {Nome}
+                </p>
             </main>
         </>
     )
