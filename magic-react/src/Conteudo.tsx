ab79d4f416c89caf2ed5fbcdb4fccf1b948bf4fb
Math.aMalafaia <math.amalafaia@gmail.com>
2026-08-13 19:41:45 -0300

    feat: Estudando React, melhora e adição da função botton. Style: mudança en algumas organização de informações

--- magic-react/src/Conteudo.tsx
+++ magic-react/src/Conteudo.tsx
@@ -1,64 +1,33 @@
 import { useState } from "react"
+import './Conteudo.css'
 
 function Conteudo(){
+    
+    const [Saiba, setSaiba] = useState("")
 
     function ClickButton(){
-        alert("Parabéns você clicou no botão")
+        setSaiba("Sou desenvolvedor web focado no momento em full-stac, apaixonado por vitoria")
     }
 
-    const ClickButton2 = () => {
-        alert("Parabéns você clicou no segundo Botão")
-    }
-    const [Nome, setNome] = useState("Sem nada")
-    const [Valor, setValor] = useState(0)
-    
-    function ClickButton3(){
-        setNome("Parabéns você clicou no quarto botão")
-        setValor(Valor + 1)
-    }
     return(
         <>
-            <main>
+            <main className="Pricipal">
                 <h2>Meu nome é Matheus Gomes</h2>
                 <h3>Sou Desenvolvedor & psicologo</h3>
                 <a href="https://github.com/Math-aMalafaia/Estudando-React">Github</a>
 
                 
                 
-                <button onClick={ClickButton}>
-                    Clique para mostrar Matheus Gomes
+                <button className="SaibaMais" onClick={ClickButton}>
+                    Saiba mais
                 </button>
                 
-                <button onClick={ClickButton2}>
-                    Clique para mostrar Matheus Gomes
-                </button>
+                <div className="Card-SaibaMais">
+                    <p>
+                        {Saiba}
+                    </p>
+                </div>
                 
-                <button onClick={
-                    () => {
-                        alert("Parabéns você clicou no terceiro botão")
-                    }
-                }>
-                    Clique para mostrar Matheus Gomes
-                </button>
-
-                <button onClick={ClickButton3}>
-                    Clique para mostra Matheus Gomes
-                </button>
-
-                <button onMouseOver={
-                    () => {
-                        alert("Mouse por cima do botão")
-                    }
-                } >
-                    Clique para mostra Matheus Gomes
-                </button>
-
-                <p>
-                    {Nome}
-                </p>
-                <h2>
-                    {Valor}
-                </h2>
             </main>
         </>
     )
