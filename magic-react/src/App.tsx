41679c5e5d3e02d4487fd0f99a90a67ae95764a3
caua <leoncaua.santos@gmail.com>
2026-08-14 08:42:33 -0300

    Feat: estudando React, fazendo liste e compilando variaveis em listas

--- magic-react/src/App.tsx
+++ magic-react/src/App.tsx
@@ -5,8 +5,8 @@ import './App.css'
 function App() {
   return (
     <> 
-    <div className="pricipal">
-
+      <div className="pricipal">
+        
       <Header/>
       <Conteudo/>
 
--- magic-react/src/Header.tsx
+++ magic-react/src/Header.tsx
@@ -1,14 +1,21 @@
 import "./Header.css"
 
-function Header(){
+function Header() {
+let Links = [
+    {href: "https://google.com", Nome: "Home"},
+    {href: "https://youtube.com", Nome: "Sobre Mim"},
+    {href: "https://facebook.com", Nome: "Projetos"}
+  ]
   return (
     <>
     <header className="Header">
       <h1>Matheus Gomes</h1>
-      <ul className="Menu">
-        <li> <a href=""> Home </a> </li>
-        <li> <a href=""> Sobre mim </a> </li>
-        <li> <a href=""> Projetos </a> </li>
+        <ul className="Menu">
+          {
+            Links.map((LinksNow) => {
+              return <li> <a href={LinksNow.href}> {LinksNow.Nome} </a>  </li>
+            })
+          }
       </ul>
     </header>
     </>
