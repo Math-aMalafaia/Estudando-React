a281445d509007ff0d5cf6a69e64eee1f688686e
Math.aMalafaia <math.amalafaia@gmail.com>
2026-08-15 13:19:22 -0300

    Feat: Estudando React, Iniciando um novo projeto, App tarefas

---
+++ tarefas/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Sidebar from './components/Sidevar'
+
+function App() {
+  return (
+    <>
+    <header>
+      <h1>Tarefas App</h1>
+    </header>
+    <div>
+      <Sidebar/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ tarefas/src/components/Sidevar.tsx
@@ -0,0 +1,10 @@
+
+function Sidebar(){
+    return(
+        <>
+            <h1>Sidebar funcionado</h1>
+        </>
+    )
+}
+
+export default Sidebar;
